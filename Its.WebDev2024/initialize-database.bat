583111b03e7f6e413885084983299888c95b4290
Stefano Vuerich <vuerich@irsweb.it>
2024-02-07 10:51:40 +0100

    fix: int db script

--- Its.WebDev2024/initialize-database.bat
+++ Its.WebDev2024/initialize-database.bat
@@ -1,2 +1,2 @@
-dotnet ef migrations add ApplicationDbContextMigration --context ApplicationDbContext
+dotnet ef migrations add InitialMigration --context ApplicationDbContext
 dotnet ef database update
